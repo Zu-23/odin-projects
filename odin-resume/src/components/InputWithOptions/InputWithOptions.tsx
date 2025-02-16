21560629da2bb98545fdea289003f00aa88ca1e9
zu-23 <h.zakaria52@gmail.com>
2025-02-16 02:10:14 +0100

    set plan

--- odin-resume/src/components/InputWithOptions/InputWithOptions.tsx
+++ odin-resume/src/components/InputWithOptions/InputWithOptions.tsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
+import InptOpt from "../InptOpt/InptOpt";
+
+type eduData = {
+  tite: string;
+  subtitle: string;
+  description: string;
+};
 
 export default function InputWithOptions() {
+  const [eduDataVar, setEduData] = useState<eduData[]>([]);
   const [showOpt, setShowOpt] = useState(false);
   function showInputs() {
     setShowOpt(!showOpt);
     console.log(showOpt);
-}
+  }
   return (
     <div className="btn-inp">
+      {showOpt && <InptOpt  />}
       <button type="button" onClick={showInputs}>
-        click me
+        New Item
       </button>
-      
     </div>
   );
 }
-
+//we gonna need to send data for education to child
+//first time we press new item we send data to InptOpt to populate
+//then we check if array of data is not empty to show education elements
+//
